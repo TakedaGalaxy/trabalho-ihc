5ba39641ac3c01514dba2180c70fa19d54ebfcbf
LuizRedGalaxy <luizgustavoglt@gmail.com>
2023-06-27 11:58:02 -0300

    Adicionado container

---
+++ src/componentes/estruturas/container/container.jsx
@@ -0,0 +1,5 @@
+import "./container.scss";
+
+export default function EstruturaContainer({ children }) {
+  return <div className="Container">{children}</div>;
+}
--- src/paginas/pesquisar/pesquisar.jsx
+++ src/paginas/pesquisar/pesquisar.jsx
@@ -1,9 +1,10 @@
+import EstruturaContainer from "../../componentes/estruturas/container/container";
 import "./pesquisar.scss";
 
 export default function PaginaPesquisar() {
   return (
-    <>
+    <EstruturaContainer>
       <h1>Pesquisar</h1>
-    </>
+    </EstruturaContainer>
   );
 }
